3e802cf5ecb022572e7072e24bb7c5891b6a4527
Nguyen Thanh Trung <luahantinhthu9@gmail.com>
2021-10-10 02:24:41 +0800

    Add project files.

---
+++ SetProxy/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿
+namespace SetProxy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnSetProxy = new System.Windows.Forms.Button();
+            this.txbProxy = new System.Windows.Forms.TextBox();
+            this.btnRemoveProxy = new System.Windows.Forms.Button();
+            this.lbResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSetProxy
+            // 
+            this.btnSetProxy.Location = new System.Drawing.Point(46, 121);
+            this.btnSetProxy.Name = "btnSetProxy";
+            this.btnSetProxy.Size = new System.Drawing.Size(103, 23);
+            this.btnSetProxy.TabIndex = 0;
+            this.btnSetProxy.Text = "Set Proxy";
+            this.btnSetProxy.UseVisualStyleBackColor = true;
+            this.btnSetProxy.Click += new System.EventHandler(this.btnSetProxy_Click);
+            // 
+            // txbProxy
+            // 
+            this.txbProxy.Location = new System.Drawing.Point(12, 56);
+            this.txbProxy.Name = "txbProxy";
+            this.txbProxy.Size = new System.Drawing.Size(315, 20);
+            this.txbProxy.TabIndex = 1;
+            // 
+            // btnRemoveProxy
+            // 
+            this.btnRemoveProxy.Location = new System.Drawing.Point(173, 121);
+            this.btnRemoveProxy.Name = "btnRemoveProxy";
+            this.btnRemoveProxy.Size = new System.Drawing.Size(103, 23);
+            this.btnRemoveProxy.TabIndex = 2;
+            this.btnRemoveProxy.Text = "Remove Proxy";
+            this.btnRemoveProxy.UseVisualStyleBackColor = true;
+            this.btnRemoveProxy.Click += new System.EventHandler(this.btnRemoveProxy_Click);
+            // 
+            // lbResult
+            // 
+            this.lbResult.AutoSize = true;
+            this.lbResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbResult.Location = new System.Drawing.Point(62, 89);
+            this.lbResult.Name = "lbResult";
+            this.lbResult.Size = new System.Drawing.Size(51, 16);
+            this.lbResult.TabIndex = 3;
+            this.lbResult.Text = "Result";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(339, 182);
+            this.Controls.Add(this.lbResult);
+            this.Controls.Add(this.btnRemoveProxy);
+            this.Controls.Add(this.txbProxy);
+            this.Controls.Add(this.btnSetProxy);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Set Proxy";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSetProxy;
+        private System.Windows.Forms.TextBox txbProxy;
+        private System.Windows.Forms.Button btnRemoveProxy;
+        private System.Windows.Forms.Label lbResult;
+    }
+}
+
---
+++ SetProxy/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Net;
+
+namespace SetProxy
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("wininet.dll")]
+        public static extern bool InternetSetOption(IntPtr hInternet, int dwOption, IntPtr lpBuffer, int dwBufferLength);
+        public const int INTERNET_OPTION_SETTINGS_CHANGED = 39;
+        public const int INTERNET_OPTION_REFRESH = 37;
+        static bool settingsReturn, refreshReturn;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SetContentResult("      Chưa kết nối proxy!", false);
+            try
+            {
+                RegistryKey registry = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings", true);
+                if (registry.GetValue("ProxyEnable").ToString() == "1")
+                    SetContentResult("Đang kết nối: " + registry.GetValue("ProxyServer").ToString());
+            }
+            catch
+            {
+                SetContentResult("          Có lỗi xảy ra!!!", false);
+            }
+        }
+
+        private void btnSetProxy_Click(object sender, EventArgs e)
+        {
+            txbProxy.Text = txbProxy.Text.Trim();
+
+            bool isChangeProxy = false;
+            string proxy = txbProxy.Text.Trim();
+
+            if (!string.IsNullOrEmpty(proxy) && proxy.Contains(":"))
+            {
+                if(proxy.Split('.').Length == 4 && proxy.Split(':').Length == 2)
+                {
+                    if(!string.IsNullOrEmpty(proxy.Split(':')[0]) && !string.IsNullOrEmpty(proxy.Split(':')[1]))
+                    {
+                        RegistryKey registry = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings", true);
+                        registry.SetValue("ProxyEnable", 1);
+                        registry.SetValue("ProxyServer", proxy);
+
+                        // These lines implement the Interface in the beginning of program 
+                        // They cause the OS to refresh the settings, causing IP to realy update
+                        settingsReturn = InternetSetOption(IntPtr.Zero, INTERNET_OPTION_SETTINGS_CHANGED, IntPtr.Zero, 0);
+                        refreshReturn = InternetSetOption(IntPtr.Zero, INTERNET_OPTION_REFRESH, IntPtr.Zero, 0);
+
+                        SetContentResult("Thành công: " + proxy);
+                        isChangeProxy = true;
+                    }
+                }
+            }
+            
+            if(!isChangeProxy)
+                SetContentResult("          Proxy không hợp lệ", false);
+        }
+        private void btnRemoveProxy_Click(object sender, EventArgs e)
+        {
+            txbProxy.Text = txbProxy.Text.Trim();
+
+            RegistryKey registry = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings", true);
+            registry.SetValue("ProxyServer", string.Empty);
+            registry.SetValue("ProxyEnable", 0);
+
+            // These lines implement the Interface in the beginning of program 
+            // They cause the OS to refresh the settings, causing IP to realy update
+            settingsReturn = InternetSetOption(IntPtr.Zero, INTERNET_OPTION_SETTINGS_CHANGED, IntPtr.Zero, 0);
+            refreshReturn = InternetSetOption(IntPtr.Zero, INTERNET_OPTION_REFRESH, IntPtr.Zero, 0);
+
+            SetContentResult("             Đã tắt Proxy", false);
+        }
+
+        private void SetContentResult(string result, bool isSuccess = true)
+        {
+            if (isSuccess)
+                lbResult.ForeColor = Color.Green;
+            else
+                lbResult.ForeColor = Color.Red;
+
+            lbResult.Text = result;
+            lbResult.Visible = true;
+        }
+
+
+    }
+}
